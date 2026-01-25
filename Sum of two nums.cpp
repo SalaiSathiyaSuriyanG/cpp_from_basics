79304e92513983aa9fb3a0ca53eaef43f314843c
SalaiSathiyaSuriyanG <salai13102005@gmail.com>
2026-01-25 15:00:12 +0530

    Add files via upload

---
+++ Sum of two nums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter value for A: ";
+cin>>a;
+cout<<"Enter value for B: ";
+cin>>b;
+c=a+b;
+cout<<"The sum is : "<<c<<endl;
+return 0;
+}
