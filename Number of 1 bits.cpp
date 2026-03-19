53a22aa5b5a3d2d4dc7af4b9dcc77beca03de46c
Salai_Sathiya_Suriyan_G <salai13102005@gmail.com>
2026-03-19 22:49:05 +0530

    Add files via upload

---
+++ Number of 1 bits.cpp
@@ -0,0 +1,46 @@
+
+/* ==================== LeetCode version =========================
+
+- Time Complexity: O(n)
+- Space Complexity: O(1)
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while(n){
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+};
+=============================================================== */
+
+// ==================== Runnable Version =========================
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while(n){
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+};
+
+int main(){
+    Solution sol;
+    int n;
+    cout <<"Enter an unsigned integer: ";
+    cin >> n;
+    
+    cout <<"The number of '1' bits in "<<n<<" is : "<<sol.hammingWeight(n);
+
+    return 0;
+}
