1fb9f827433a288372f53d1257d7f43c6b9c6cf8
Salai_Sathiya_Suriyan_G <salai13102005@gmail.com>
2026-03-29 20:46:03 +0530

    Contains duplicate
    Refactored the containsDuplicate function to be public and removed duplicate code.

--- Contains duplicate.cpp
+++ Contains duplicate.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Solution{
+public:
     bool containsDuplicate(vector<int>& nums){
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
